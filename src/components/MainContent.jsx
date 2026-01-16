7fbb80ef390b3787d7d3fab208a5905390dd72dc
Clara Loforti <clara.loforti@gmail.com>
2026-01-16 18:09:16 +0100

    footer lists

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -27,54 +27,6 @@ const MainContent = () => {
                     </ul>
                 </div>
             </section>
-            <section className="section-lists">
-                <div className="container">
-                    <div className="el-1">
-                        <h3>DC COMICS</h3>
-                        <ul>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                        </ul>
-                        <h3>SHOP</h3>
-                        <ul>
-                            <li>Shop DC</li>
-                            <li>Shop DC Collectibles</li>
-                        </ul>
-                    </div>
-                    <div className="el-2">
-                        <h3>DC</h3>
-                        <ul>
-                            <li>Terms Of Use</li>
-                            <li>Privacy policy (New)</li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshops</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                            <li>Shop Help</li>
-                            <li>Contact Us</li>
-                        </ul>
-                    </div>
-                    <div className="el-3">
-                        <h3>SITES</h3>
-                        <ul>
-                            <li>DC</li>
-                            <li>MAD Magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>DC Power Visa</li>
-                        </ul>
-                    </div>
-                </div>
-
-            </section>
         </main>
     )
 }

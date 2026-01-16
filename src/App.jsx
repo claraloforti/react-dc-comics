66f19c2e1873bae2569ade4f56a38fcff84390d9
Clara Loforti <clara.loforti@gmail.com>
2026-01-16 13:51:55 +0100

    reset css, aggiungo font open sans al body, preparo cartella components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MainHeader from "./components/MainHeader"
+import MainContent from "./components/MainContent"
+import MainFooter from "./components/MainFooter"
+
+function App() {
+  return (
+    <>
+      <MainHeader />
+      <MainContent />
+      <MainFooter />
+    </>
+  )
+}
+
+export default App

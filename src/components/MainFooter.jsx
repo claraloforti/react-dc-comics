60d23572c987d46365dff5bbca6525a8205c60fe
Clara Loforti <clara.loforti@gmail.com>
2026-01-17 19:19:03 +0100

    bottom-bar

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -51,7 +51,15 @@ const MainFooter = () => {
             </div>
             <div className="bottom-bar">
                 <div className="container">
-                    <p>ciao</p>
+                    <button className="signup-btn">SIGN-UP NOW!</button>
+                    <div className="contacts">
+                        <p>FOLLOW US</p>
+                        <img src="img/footer-facebook.png" alt="" />
+                        <img src="img/footer-twitter.png" alt="" />
+                        <img src="img/footer-youtube.png" alt="" />
+                        <img src="img/footer-pinterest.png" alt="" />
+                        <img src="img/footer-periscope.png" alt="" />
+                    </div>
                 </div>
             </div>
         </footer>

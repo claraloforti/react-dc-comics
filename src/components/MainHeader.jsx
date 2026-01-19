07f3a1cbd58063d1cf29409cd2dfec084ee3b75d
Clara Loforti <clara.loforti@gmail.com>
2026-01-19 17:59:55 +0100

    creo componente navbar

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,43 +1,14 @@
+import Navbar from "./Navbar";
+
 const MainHeader = () => {
     return (
         <header>
             <div className="container">
                 <img src="/img/dc-logo.png" alt="logo" className="dc-logo" />
-                <ul className="navbar">
-                    <li>CHARACTERS
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>COMICS
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>MOVIES
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>TV
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>GAMES
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>COLLECTIBLES
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>VIDEOS
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>FANS
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>NEWS
-                        <div className="barra-hover"></div>
-                    </li>
-                    <li>SHOP
-                        <div className="barra-hover"></div>
-                    </li>
-                </ul>
+                <Navbar />
             </div>
         </header>
     )
 }
 
-export default MainHeader
+export default MainHeader;
